e1f17231804cb14d5520c764a937883083411e9f
LakshyaGarg <lakshyagg2@gmail.com>
2017-11-25 11:49:48 +0530

    completion of string1 exercise

--- basic/string1.py
+++ basic/string1.py
@@ -16,6 +16,11 @@
 # are some additional functions to try in string2.py.
 
 
+
+######SUCCESFULLY COMPLETED#####
+
+
+
 # A. donuts
 # Given an int count of a number of donuts, return a string
 # of the form 'Number of donuts: <count>', where <count> is the number
